7a56f1db284a73ca9e4ddbc604b8f7dad959e7d3
HardiB.Salih <30411248+HardiB-Salih@users.noreply.github.com>
2023-07-14 22:11:48 +0300

    Initial Commit

---
+++ RiveAnimated/RiveAnimatedApp.swift
@@ -0,0 +1,17 @@
+//
+//  RiveAnimatedApp.swift
+//  RiveAnimated
+//
+//  Created by HardiB.Salih on 7/14/23.
+//
+
+import SwiftUI
+
+@main
+struct RiveAnimatedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
